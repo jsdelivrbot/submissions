4d2bd47d9d03c561c9f00c41d38fb6e4d6c902da
sydneyacaputo <sydneyacaputo@gmail.com>
2015-02-09 17:52:24 -0500

    a

--- 6/canvas/sydcaputo/canvas.js
+++ 6/canvas/sydcaputo/canvas.js
@@ -4,10 +4,11 @@ var c= document.getElementById("c");
 var ctx = c.getContext("2d");
 
 
+
 var clear = function(e){
 		ctx.fillStyle = "#ffffff";
 		ctx.fillRect(0,0,600,600);
-		//ctx.beginPath();
+		ctx.beginPath();
 };
 c.addEventListener("click",clicked);
 var b = document.getElementById("b");
