131ed1373f08973390cb4cccbe0f1bead5839bd8
sydneyacaputo <sydneyacaputo@gmail.com>
2015-02-09 17:52:17 -0500

    added files

---
+++ 6/canvas/sydcaputo/canvas.js
@@ -0,0 +1,14 @@
+
+
+var c= document.getElementById("c");
+var ctx = c.getContext("2d");
+
+
+var clear = function(e){
+		ctx.fillStyle = "#ffffff";
+		ctx.fillRect(0,0,600,600);
+		//ctx.beginPath();
+};
+c.addEventListener("click",clicked);
+var b = document.getElementById("b");
+b.addEventListener("click",clear);
