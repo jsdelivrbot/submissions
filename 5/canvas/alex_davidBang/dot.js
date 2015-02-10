b9e92f7ff3177a020555117df6cd72c184b6fb2b
Alex Libman <kingalex11235@yahoo.com>
2015-02-10 17:35:32 -0500

    more work

--- 5/canvas/alex_davidBang/dot.js
+++ 5/canvas/alex_davidBang/dot.js
@@ -1,52 +1,53 @@
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 var mouse = {x: 0, y: 0};
-
+var dots = [];
 
 var makeDot = function(ctx){
     return {
-	x : 100 + 800*Math.random(),
-	y : 500,
-	r : 15,
-	ctx : ctx,
-	dx : -1,
-	dy : -1,
-	color : "#ff0000",
-	spawn : function() {
-	    e.preventDefault();
-	    ctx.arc(x,y,r,0,2*Math.PI);
-	    ctx.fillStyle= color;
-	    ctx.stroke();
-	    ctx.fill();
-	},
-	move : function() {
-	    this.x = this.x + dx;
-	    this.y = this.y + dy;
-	}
-	checkCollide : function() {
-	    return (Math.pow(mouse.x - this.x, 2) + Math.pow(mouse.y - this.y, 2) <= Math.pow(this.r,2)); 
-	}
-	inBounds : function() {
-	    return (this.x > 0 && this.y > 0);
-	}
+	    x : 20 + 1200*Math.random(),
+	    y : -15,
+	    r : 15,
+	    ctx : ctx,
+	    dx : -2,
+	    dy : 2,
+	    color : "#ff0000",
+	    spawn : function() {
+            ctx.beginPath();
+	        ctx.arc(this.x,this.y,this.r,0,2*Math.PI);
+	        ctx.fillStyle= this.color;
+	        ctx.stroke();
+	        ctx.fill();
+	    },
+	    move : function() {
+	        this.x = this.x + this.dx;
+	        this.y = this.y + this.dy;
+	    },
+	    checkCollide : function() {
+	        return (Math.pow(mouse.x - this.x, 2) + Math.pow(mouse.y - this.y, 2) <= Math.pow(this.r,2)); 
+	    },
+	    inBounds : function() {
+	        return (this.x > -this.r && this.y < 500 + this.r);
+	    }
     };
 };
 
-
 var update = function() {
     ctx.fillStyle="#ffffff";
-    var b = makeDot(ctx);
-    dots.push(b);
+    ctx.fillRect(0,0,900,500);
+    if (dots.length < 25){
+        dots.push(makeDot(ctx));
+    };
     for (var i = 0; i < dots.length; i++){
-	dots[i].move();
-	dots[i].spawn();
-	if (dot[i].checkCollide()) {
-	    console.log ("NOOB. GET BETTER");
-	}
-	if (!(dot[i].inBounds())) {
-	    dots.pop(dot[i]);
-	}
-    }
+	    dots[i].move();
+	    dots[i].spawn();
+	    /*if (dots[i].checkCollide()) {
+	        console.log("NOOB. GET BETTER");
+	    }*/
+	    if (!(dots[i].inBounds())) {
+	        dots.splice(i,1);
+	    }
+    };
     window.requestAnimationFrame(update);
 };
 
@@ -56,10 +57,7 @@ document.addEventListener('mousemove', function(e){
     mouse.y = e.clientY || e.pageY;
 }, false);
 
-
-
-var dots = [];
-dots.push(makeDot(50,100,30,15,ctx));
-dots.push(makeDot(100,200,30,15,ctx));
-c.addEventListener("click",clicked);
+//dots.push(makeDot(ctx));
+//dots.push(makeDot(ctx));
+//c.addEventListener("click",clicked);
 window.requestAnimationFrame(update);
