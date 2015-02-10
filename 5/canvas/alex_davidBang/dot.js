32ffe1dbc36a292a340c5c3894e3841f2ff328df
Alex Libman <kingalex11235@yahoo.com>
2015-02-10 18:58:02 -0500

    more work

--- 5/canvas/alex_davidBang/dot.js
+++ 5/canvas/alex_davidBang/dot.js
@@ -3,12 +3,14 @@ var ctx = c.getContext("2d");
 var mouse = {x: -1, y: -1};
 var dots = [];
 var lost = false;
+var score = 0;
+var i = 0;
 
 var makeDot = function(ctx){
     return {
 	    x : 20 + 1200*Math.random(),
 	    y : -15,
-	    r : 15,
+	    r : 12 + 6*Math.random(),
 	    ctx : ctx,
 	    dx : -3 + 2*Math.random(),
 	    dy : 2 + 2*Math.random(),
@@ -33,24 +35,44 @@ var makeDot = function(ctx){
     };
 };
 
+var setup = function(){
+    ctx.fillStyle="#000000";
+    ctx.fillRect(0,0,900,500);
+    while (dots.length < 30){
+            dots.push(makeDot(ctx));
+    };
+}
+
 var update = function() {
     if (!(lost || mouse.x < 0 || mouse.x > 900 || mouse.y < 0 || mouse.y > 500)){
-        ctx.fillStyle="#ffffff";
-        ctx.fillRect(0,0,900,500);
-        if (dots.length < 20){
+        //i++;
+        console.log(i);
+        if (dots.length < 40 + score / 500){
             dots.push(makeDot(ctx));
+            //i = 0;
         };
+        ctx.fillStyle="#000000";
+        ctx.fillRect(0,0,900,500);
         for (var i = 0; i < dots.length; i++){
 	        dots[i].move();
 	        dots[i].spawn();
 	        if (dots[i].checkCollide()) {
                 lost = true;
-	            console.log("NOOB. GET BETTER");
 	        }
 	        if (!(dots[i].inBounds())) {
 	            dots.splice(i,1);
 	        }
         };
+        ctx.font="20px Arial";
+        ctx.fillStyle="#ffffff";
+        ctx.fillText("Score: ",760,490);
+        ctx.fillText(score,830,490);
+        if (lost){
+	        ctx.font="90px Georgia";
+            ctx.fillStyle="#ffffff";
+            ctx.fillText("Game Over!",180,265);
+        };
+        score += 1;
     };
     window.requestAnimationFrame(update);
 };
@@ -61,7 +83,5 @@ document.addEventListener('mousemove', function(e){
     mouse.y = e.clientY - c.offsetTop;
 }, false);
 
-//dots.push(makeDot(ctx));
-//dots.push(makeDot(ctx));
-//c.addEventListener("click",clicked);
+setup();
 window.requestAnimationFrame(update);
