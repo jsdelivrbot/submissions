fbe856b141d459a90ef248be091b2c267a466d19
Justin Kim <yulli@yulli.org>
2015-02-10 00:02:38 -0500

    ¯\_(ツ)_/¯

---
+++ 5/canvas/justin_kim/input.js
@@ -0,0 +1,31 @@
+APP.setConsts({
+    K_UP : "W",
+    K_DOWN : "S",
+    K_LEFT : "A",
+    K_RIGHT : "D"
+});
+
+// These will be set to true if the corresponding input is pressed
+APP.setConsts({
+    I_UP : false,
+    I_DOWN : false,
+    I_LEFT : false,
+    I_RIGHT : false
+});
+
+
+var keyDown = function(event) {
+    var key = String.fromCharCode(event.keyCode);
+    if (key == APP.K_UP)    APP.I_UP = true;
+    if (key == APP.K_DOWN)  APP.I_DOWN = true;
+    if (key == APP.K_LEFT)  APP.I_LEFT = true;
+    if (key == APP.K_RIGHT) APP.I_RIGHT = true;
+}
+
+var keyUp = function(event) {
+    var key = String.fromCharCode(event.keyCode);
+    if (key == APP.K_UP)    APP.I_UP = false;
+    if (key == APP.K_DOWN)  APP.I_DOWN = false;
+    if (key == APP.K_LEFT)  APP.I_LEFT = false;
+    if (key == APP.K_RIGHT) APP.I_RIGHT = false;
+}
