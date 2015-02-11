8ca6a906e6da5f0c4d4b8ea0c36afc831785aec1
Genji Noguchi <genji.noguchi@gmail.com>
2015-02-10 23:30:25 -0500

    Ball movement workign

--- 7/canvas/genji_fawn/src/ball.js
+++ 7/canvas/genji_fawn/src/ball.js
@@ -3,10 +3,12 @@
  *
  */
 
-Portal.Ball = function(x, y, color, r, isPlayer) {
+Portal.Ball = function(x, y, color, r, vx, vy, isPlayer) {
 	this.x 			= x			? x		: X_DEFAULT;
-	this.x			= y			? y		: Y_DEFAULT;
+	this.y			= y			? y		: Y_DEFAULT;
 	this.r 			= r			? r		: R_DEFAULT;
+	this.vx 		= vx		? vx	: R_DEFAULT;
+	this.vy			= vy		? vy	: R_DEFAULT;
 	this.color 		= color		? color	: C_DEFAULT;	
 	this.isPlayer	= isPlayer	? isPlayer:false;
 	
@@ -14,5 +16,19 @@ Portal.Ball = function(x, y, color, r, isPlayer) {
 	var Y_DEFAULT = Portal.height/2;
 	var R_DEFAULT = 10;
 	var C_DEFAULT = "#0066FF";
+	var VX_DEFAULT = 0;
+	var VY_DEFAULT = 0;
 
+	this.draw = function(ctx) {	
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+		ctx.fillStyle = this.color;
+		ctx.fill();
+		ctx.closePath();
+	}
+
+	this.update = function() {
+		this.x += this.vx;
+		this.y += this.vy;
+	}
 };
--- 7/canvas/genji_fawn/src/sprites.js
+++ 7/canvas/genji_fawn/src/sprites.js
@@ -4,32 +4,34 @@ Portal.sprites = (function() {
 	this.numPlayers = 0;
 
 	function draw(ctx) {
-		for (ball in balls) {
-			//ball.draw(ctx);	
-			ctx.beginPath();
-			ctx.arc(ball.x, ball.y, ball.r, 0, 2*Math.PI);
-			ctx.fillStyle(ball.color);
-			ctx.fill();
-			ctx.closePath();
+		for (i in balls) {
+			//console.log(balls[i]);
+		
+			/* Draw the ball */
+			balls[i].draw(ctx);
+
+			/* Update the ball position */
+			balls[i].update();
 		}
 	}
 
 	/* Ball Creation and Manipulation */
-	function createBall(x, y, color, r, isPlayer) {
+	function createBall(x, y, color, r, vx, vy, isPlayer) {
 		if(isPlayer) {
 			if( numPlayers == 0 ) {
 				numPlayers = 0;
-				balls.push(new Portal.Ball(x, y, color, r, true));
+				balls.push(new Portal.Ball(x, y, color, r, vx, vy, true));
 				return;
 			} else {
 				console.log("A player was already created.");
 			}
 		}
-		balls.push(new Portal.Ball(x, y, color, r, false));
+		balls.push(new Portal.Ball(x, y, color, r, vx, vy, false));
 	}
 
 	return {
 		draw		: draw,
-		createBall	: createBall
+		createBall	: createBall,
+		balls		: this.balls
 	}
 })();
