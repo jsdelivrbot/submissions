ab53a7fc48ebcf564f6e9d585d9a90b5289169d4
Ling Dong <lingd1944@gmail.com>
2015-02-10 20:18:33 -0500

    some work not workin

--- 6/canvas/cindy-ling/canvas.js
+++ 6/canvas/cindy-ling/canvas.js
@@ -3,26 +3,82 @@ var ctx = c.getContext("2d");
 
 
 
-ctx.beginPath();
-ctx.arc(650,300,300,0,2*Math.PI);
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.stroke();
+//ctx.beginPath();
+//ctx.arc(650,300,300,0,2*Math.PI);
+//ctx.fillStyle = "red";
+//ctx.fill();
+//ctx.stroke();
+
+var makeCircle = function(x, y, r,ctx){
+    return {
+	x : x,
+	y : y,
+	r : r,
+	color : "#FA9961",
+	ctx : ctx,
+	dx : 1,
+	dy : 1,
+	dr : 1,
+	draw : function() {
+	    ctx.beginPath();
+	    ctx.arc(x, y, r, 0, 2*Math.PI);
+	    ctx.fillStyle = this.color;
+	    ctx.fill();
+	    ctx.stroke();
+	}
+	move : function(){
+	    this.x = this.x + this.dx;
+	    this.y = this.y + this.dy;
+	    if (this.r <= 0){
+		dr = dr * -1;
+	    }
+	    if (this.x <= 10 || this.x > 1290){
+		this.dx = this.dx * -1;
+		this.r = this.r - dr;
+	    }
+	    if (this.y < 10 || this.y > 590){
+		this.dy = this.dy * -1;
+		this.r = this.r - dr;
+	    }
+	}
+    }
+}
+
+var update = function(){
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0,0,1300,600);
+    for (var i = 0; i < balls.length; i++){
+	balls[i].move();
+	balls[i].draw();
+    }
+    window.requestAnimationFrame(update);
+}
 
 var clicked = function(e) {
-    console.log(b);
     e.preventDefault();
-    ctx.arc(650,150,150,0,2*Math.PI);
-    ctx.arc(650,150,150,0,2*Math.PI);
-    ctx.fillStyle = "red";
-    ctx.fill();
-    ctx.stroke();
-
-};
-
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var r = 10 + Math.random()*75;
+    balls.push(makeCircle(x, y, r, ctx));
+    //console.log(b);
+    //e.preventDefault();
+    //ctx.arc(650,150,150,0,2*Math.PI);
+    //ctx.arc(650,150,150,0,2*Math.PI);
+    //ctx.fillStyle = "red";
+    //ctx.fill();
+    //ctx.stroke();
+}
+	       
 var clear = function(e) {
-};
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0,0,1300,600);
+}
 
 c.addEventListener("click", clicked);
-var b = document.getElementById("dup");
+var balls = [];
+window.requestAnimationFrame(update);
+    
+    
+var b = document.getElementById("butt");
+b.addEventListener("click", clear);
 
