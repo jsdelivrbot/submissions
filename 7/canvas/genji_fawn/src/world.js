c5b91747aa2892e9433e8f7cc84194e88560b611
fawnwong <fawn.wong@live.com>
2015-02-10 23:54:40 -0500

    i'm too tired for this

--- 7/canvas/genji_fawn/src/world.js
+++ 7/canvas/genji_fawn/src/world.js
@@ -5,7 +5,7 @@ Portal.world = (function() {
 	var WIDTH	 		= 300,
 		HEIGHT	 		= 150,
 		COLOR			= "#E6E6E6",
-		PORTALHEIGHT 	= 50;
+		PORTALHEIGHT 	= 25;
 
 	var elements = [];
 
@@ -14,10 +14,10 @@ Portal.world = (function() {
 		ctx.fillRect(0, 0, WIDTH, HEIGHT );
 		//blue
 		ctx.fillStyle = "#00ffff";
-		ctx.fillRect(0, (HEIGHT * 0.5) - PORTALHEIGHT, 5, (HEIGHT * 0.5) + PORTALHEIGHT);
+		ctx.fillRect(0, PORTALHEIGHT, 5, (HEIGHT * 0.5) + PORTALHEIGHT);
 		//orange
 		ctx.fillStyle = "#ffa500";
-		ctx.fillRect(WIDTH - 5, (HEIGHT * 0.5) - PORTALHEIGHT, WIDTH, (HEIGHT * 0.5) + PORTALHEIGHT);
+		ctx.fillRect(WIDTH - 5, PORTALHEIGHT, WIDTH, (HEIGHT * 0.5) + PORTALHEIGHT);
 	}
 
 	function setDimensions(canvas) {
