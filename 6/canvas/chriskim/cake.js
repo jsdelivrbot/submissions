a948c74fe500d38e3ea83b42a43bad295b858007
chriskim511 <christopherkim511@gmail.com>
2015-02-11 15:57:55 -0500

    cakes

--- 6/canvas/chriskim/cake.js
+++ 6/canvas/chriskim/cake.js
@@ -1,55 +1,59 @@
+var b = document.getElementById("b");
 var c = document.getElementById("c");
 var ctx = c.getContext("2d");
 
-var makeBlock = function(x,y,w,h,ctx) {
-		return {
-				x : x,
-				y : y,
-				w : w,
-				h : h,
-				ctx : ctx,
-				color : "#ff0000",
-				dx : 1,
-				draw : function() {
-						ctx.fillStyle = this.color;
-						ctx.fillRect(this.x,this.y,this.w,this.h);
-				},
-				move : function() {
-						this.x = this.x + this.dx;
-						this.y = this.y + 2*Math.random() - 1;
-						if (this.x < 10 || this.x > 580){
-								this.dx = this.dx * -1;
-						}
-						if (this.y < 20 || this.y > 580){
-								this.y = 100 + 400 * Math.random();
-						}
-				}
-						
-				
-		};
+var cake = [];
+
+var addCake = function(x,y,w,h,dx,dy,ctx,pic){
+    //console.log("add");
+    return{
+	x: x,
+	y: y,
+	w: w,
+	h: h,
+	ctx: ctx,
+	dx: dx,
+	dy: dy,
+	pic: pic,
+	draw: function(){
+	    var cake = new Image();
+	    cake.src = this.pic;
+	    ctx.drawImage(cake,this.x,this.y,this.w,this.h);
+	},
+	move: function(){
+	    this.x = this.x + this.dx;
+	    this.y = this.y + this.dy;
+	    if (this.x < 10 || this.x > 515){
+		this.dx = this.dx * -1;
+	    }
+	    if (this.y < 10 || this.y > 510){
+		this.dy = this.dy * -1;
+	    }
+	},
 };
 
-var update = function() {
-		ctx.fillStyle = "#ffffff";
-		ctx.fillRect(0,0,600,600);
-		for (var i = 0; i < blocks.length; i++){
-				blocks[i].move();
-				blocks[i].draw();
-		}
-		window.requestAnimationFrame(update);
+var update = function(){
+    //console.log("update");
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0,0,600,600);
+    for (var i = 0; i < cake.length; i++){
+	cake[i].move();
+	cake[i].draw();
+    }
+    window.requestAnimationFrame(update);
 };
 
 var clicked = function(e){
-		var x = e.offsetX;
-		var y = e.offsetY;
-		var w = 10+Math.random()*40;
-		var h = 10+Math.random()*20;
-		blocks.push(makeBlock(x,y,w,h,ctx));
+    //console.log("click");
+    var x = e.offsetX;
+    var y = e.offsetY;
+    var w = 22 + Math.random() * 22;
+    var h = 22 + Math.random() * 22;
+    var dx = 0.4 + Math.random();
+    var dy = 0.4 + Math.random();
+    cake.push(addCake(x,y,w,h,dx,dy,ctx,"cake.jpg"));
 };
 
-c.addEventListener("click",clicked);
-var blocks = [];
-blocks.push(makeBlock(50,100,30,15,ctx));
-blocks.push(makeBlock(100,200,30,15,ctx));
+c.addEventListener("click", clicked);
+
 window.requestAnimationFrame(update);
-						
