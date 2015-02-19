0975360a4f7a2e1da2bbab2eaaad828bd379049b
RzhanRichardZhan <zhan97richard@gmail.com>
2015-02-19 14:51:44 -0500

    Tentatively done

--- 7/svg/richard/svg.js
+++ 7/svg/richard/svg.js
@@ -2,15 +2,21 @@ var s = document.getElementById("s");
 var circles = []
 var player = 0;
 var frames = 0;
+var distance = function(x0,y0,x1,y1){
+    return Math.sqrt(Math.pow((x1-x0),2) + Math.pow((y1-y0),2));
+}
 var mouseOverIt = function(e){
     if ((parseInt(player.getAttribute("cx")) + parseInt(player.getAttribute("r")) + 1 < 600 && parseInt(player.getAttribute("cx")) - parseInt(player.getAttribute("r")) - 1 > 0)
 	&&
-	(parseInt(player.getAttribute("cy")) + parseInt(player.getAttribute("r")) + 1 < 600 && parseInt(player.getAttribute("cy")) - parseInt(player.getAttribute("r")) - 1 > 0)){
+	(parseInt(player.getAttribute("cy")) + parseInt(player.getAttribute("r")) + 1 < 600 && parseInt(player.getAttribute("cy")) - parseInt(player.getAttribute("r")) - 1 > 0)
+        &&
+	player.getAttribute("fill") == "#0000FF"
+       ){
     player.setAttribute("r",parseInt(player.getAttribute("r"))+1);
     }
 }
 //var createCircle = function(x,y,r,f,g,dx,dy){
-var createCircle = function(x,y,r,f,g,t){
+var createCircle = function(x,y,r,f,t){
     var c = document.createElementNS("http://www.w3.org/2000/svg","circle");
     c.setAttribute("cx",String(x));
     c.setAttribute("cy",String(y));
@@ -28,7 +34,6 @@ var createCircle = function(x,y,r,f,g,t){
 	//dx: dx,
 	//dy: dy,
 	t : t,
-	g : g,
 	move : function(e){
 	    //console.log("Wtf");
 	    // if (parseInt(this.c.getAttribute("cx")) + parseInt(this.c.getAttribute("r")) > 600 || parseInt(this.c.getAttribute("cx")) - parseInt(this.c.getAttribute("r")) < 0){
@@ -62,9 +67,38 @@ var createCircle = function(x,y,r,f,g,t){
 		    this.c.setAttribute("cy",this.c.getAttribute("r"));
 		}
 	    }
+	    var bump = 0;
+	    for (var i = 0; i < circles.length; i++){
+		var x0 = parseInt(this.c.getAttribute("cx"));
+		var x1 = parseInt(circles[i].c.getAttribute("cx"));
+		var y0 = 600-parseInt(this.c.getAttribute("cy"));
+		var y1 = 600-parseInt(circles[i].c.getAttribute("cy"));
 		
-	    this.c.setAttribute("cx",Math.round(parseInt(this.c.getAttribute("cx")) + Math.cos(this.t)*5));
-	    this.c.setAttribute("cy",Math.round(parseInt(this.c.getAttribute("cy")) - Math.sin(this.t)*5));
+		if (this != circles[i]
+		    && distance(x0,y0,x1,y1) <= parseInt(this.c.getAttribute("r")) + parseInt(circles[i].c.getAttribute("r"))){
+		    var slope = (y1-y0)/(x1-x0);
+		    var angle = Math.atan(slope);
+		    if (angle < 0){
+			angle += 2*Math.PI;
+		    }
+		    var antiangle = Math.atan(slope) + Math.PI;
+		    if (antiangle >= 2*Math.PI){
+			antiangle-=2*Math.PI;
+		    }
+		    if (!((y1 >= y0 && x1 >= x0) ||
+			(y1 <= y0 && x1 >= x0))){
+			this.t = angle;
+			circles[i].t = antiangle;
+		    }
+		    else{
+			this.t = antiangle;
+			circles[i].t = angle;
+		    }
+		    bump = 1;
+		}
+	    }		
+	    this.c.setAttribute("cx",Math.round(parseInt(this.c.getAttribute("cx")) + Math.cos(this.t)*5));//+bump);
+	    this.c.setAttribute("cy",Math.round(parseInt(this.c.getAttribute("cy")) - Math.sin(this.t)*5));//-bump);
 	    //this.c.setAttribute("cx",parseInt(this.c.getAttribute("cx"))+1);
 	}
     }
@@ -85,6 +119,10 @@ var update = function(e){
     window.requestAnimationFrame(update);
 }
 
-//createCircle(300,300,10,"#000000",0,1,1);
-createCircle(300,300,10,"#000000",0,Math.PI/3);
+createCircle(580,580,20,"#000000",3*Math.PI/4);
+createCircle(300,300,10,"#0000FF",Math.PI/3);
+createCircle(100,100,10,"#FF0000",Math.PI/4);
+createCircle(500,500,30,"#FF0000",9*Math.PI/5);
+createCircle(400,250,10,"#FF0000",7*Math.PI/11);
+createCircle(580,20,20,"#FF0000",5*Math.PI/4);
 window.requestAnimationFrame(update);
