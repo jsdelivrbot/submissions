2a20d100747c9148be9ebc5cd87cdaa97f91cded
Philipp Steinmann <steinmann.philipp@yahoo.com>
2015-02-22 23:15:54 -0500

    Finish buttons

--- 7/svg/anya-philipp/script.js
+++ 7/svg/anya-philipp/script.js
@@ -29,20 +29,26 @@ var VACCINE_REGENERATION = 10;
 var MAX_VACCINE_LEVEL = 100;
 var VACCINE_COST = 100;
 var VACCINE_COLOR = "#00ff00";
+var VACCINE_HOVER_COLOR = "#00ff00";
 var VACCINE_SVG = "images/vaccine.svg";
 
 var CURE_REGENERATION = 5;
 var MAX_CURE_LEVEL = 100;
 var CURE_COST = 100;
 var CURE_COLOR = "#ff00ff";
+var CURE_HOVER_COLOR = "#ff00ff";
 var CURE_SVG = "images/cure.svg";
 
 // Fraction of toolbar dimensions
 var BUTTON_WIDTH = 0.20;
 var BUTTON_HEIGHT = 1.0;
 
+// Fraction of button dimensions
+var BUTTON_PADDING = 0.05;
+
 // In pixels, not fraction
 var DISTANCE_BETWEEN_BUTTONS = 10;
+var BUTTON_TEXT_HEIGHT = 20;
 
 var makePerson = function(x,y,w,h,dx,dy, healthStatus, people, ctx) {
     return {
@@ -297,14 +303,20 @@ var makeIntersection = function(x, y, width, height) {
     }
 }
 
-var makeToolButton = function(x, y, width, height, initial_level, color, logo_url, ctx) {
+var makeToolButton = function(name, x, y, width, height, initial_level, color, hover_color, logo_url, ctx) {
     return {
+        name : name,
         x : x,
         y : y,
         width : width,
-        height : height,
+        full_height : height,
+        button_height : height * 0.8,
+        bar_height : height * 0.2,
         level : initial_level,
-        color: color,
+        max_level : initial_level,
+        color : color,
+        hover_color : hover_color,
+        hover : false,
         logo_url : logo_url,
         logo : null,
         logo_loaded : false,
@@ -314,37 +326,95 @@ var makeToolButton = function(x, y, width, height, initial_level, color, logo_ur
             button_reference.logo_loaded = true;
         },
 
-        draw : function() {
-            ctx.strokeStyle = this.color;
-            ctx.lineWidth = 1;
-            ctx.beginPath();
-            ctx.rect(x, y, width, height);
-            //ctx.fill();
-            ctx.stroke();
+        drawButton : function() {
+            this.ctx.strokeStyle = this.color;
+            this.ctx.lineWidth = 1;
+            
+            this.ctx.fillStyle = "#ffffff";
+            if (this.hover)
+                this.ctx.fillStyle = this.hover_color;
 
+            this.ctx.beginPath();
+            this.ctx.rect(this.x, this.y, this.width, this.button_height);
+            this.ctx.fill();
+            this.ctx.stroke();
+
+            this.drawLogo();
+            this.drawText();
+        },
+
+        drawLogo : function() {
+            if (this.logo_loaded) {
+                var logo_width = 0.3 * this.width;
+                var logo_height = logo_width * (this.logo.height / this.logo.width);
+
+                var logo_left_corner = this.x + BUTTON_PADDING * this.width;
+                var logo_top_corner = this.y + BUTTON_PADDING * this.button_height;
+
+                this.ctx.drawImage(this.logo, logo_left_corner, logo_top_corner, logo_width, logo_height);
+            }
+        },
+
+        drawText : function() {
+            var text_left_corner = this.x + 0.3 * this.width + this.width * 0.1;
+
+            var button_top_corner = this.y + BUTTON_PADDING * this.button_height;
+            var vertical_space_available = (this.button_height - 2 * BUTTON_PADDING);
+            var text_bisector = button_top_corner + vertical_space_available / 2;
+            // Just makes it look better
+            text_bisector += 4;
+
+            var text_max_width = 0.7 * (this.width - 2 * BUTTON_PADDING);
+
+            this.ctx.fillStyle = "#000000";
+            this.ctx.font = BUTTON_TEXT_HEIGHT + "px Arial";
+            this.ctx.fillText(this.name, text_left_corner, text_bisector, text_max_width);
+        },
+
+        drawBar : function() {
+            var bar_left_corner = this.x;
+            var bar_top_corner = this.y + this.button_height;
+            
+            // Draw bar outline
+            this.ctx.strokeStyle = this.color;
+            this.ctx.strokeRect(bar_left_corner, bar_top_corner, this.width, this.bar_height);
+
+            var bar_width = this.width * (this.level / this.max_level);
+
+            this.ctx.strokeStyle = this.color;
+            this.ctx.fillStyle = this.color;
+            this.ctx.beginPath();
+            this.ctx.rect(bar_left_corner, bar_top_corner, bar_width, this.bar_height);
+            this.ctx.stroke();
+            this.ctx.fill();
+        },
+
+        loadLogoIfNecessary : function() {
             if (this.logo == null) {
                 this.logo = new Image();
                 var button_reference = this;
                 this.logo.onload = function() { button_reference.logoLoaded(button_reference) };
                 this.logo.src = this.logo_url;
             }
+        },
 
-            if (this.logo_loaded) {
-                var logo_width = 0.3 * this.width;
-                var logo_height = logo_width * (this.logo.height / this.logo.width);
+        checkHover : function(x, y) {
+            var right_x = this.x + this.width;
+            var bottom_y = this.y + this.full_height;
+            this.hover = (x >= this.x && x <= right_x && y >= this.y && y <= bottom_y);
+        },
 
-                var logo_left_corner = this.x + 0.05 * this.width;
-                var logo_top_corner = this.y + 0.05 * this.height;
-
-                ctx.drawImage(this.logo, logo_left_corner, logo_top_corner, logo_width, logo_height);
-            }
+        draw : function() {
+            this.drawButton();
+            this.drawBar();
+            this.loadLogoIfNecessary();
         }
     }
 }
 
 var update = function(){
     ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0,0,GAME_WIDTH, GAME_HEIGHT);
+    ctx.fillRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     for (var i = 0; i < people.length; i++) {
         people[i].move();
@@ -473,7 +543,7 @@ var createVaccineButton = function() {
     var bisector_y = TOOLBAR_Y + (TOOLBAR_HEIGHT / 2) - TOOLBAR_PADDING_VERTICAL;
     var top_corner_y = bisector_y - height / 2;
 
-    vaccine_button = makeToolButton(left_corner_x, top_corner_y, width, height, MAX_VACCINE_LEVEL, VACCINE_COLOR, VACCINE_SVG, ctx);
+    vaccine_button = makeToolButton("Vaccine", left_corner_x, top_corner_y, width, height, MAX_VACCINE_LEVEL, VACCINE_COLOR, VACCINE_HOVER_COLOR, VACCINE_SVG, ctx);
 }
 
 var createCureButton = function() {
@@ -485,7 +555,7 @@ var createCureButton = function() {
     var bisector_y = TOOLBAR_Y + (TOOLBAR_HEIGHT / 2) - TOOLBAR_PADDING_VERTICAL;
     var top_corner_y = bisector_y - height / 2;
 
-    cure_button = makeToolButton(left_corner_x, top_corner_y, width, height, MAX_VACCINE_LEVEL, CURE_COLOR, CURE_SVG, ctx);
+    cure_button = makeToolButton("Cure", left_corner_x, top_corner_y, width, height, MAX_VACCINE_LEVEL, CURE_COLOR, CURE_HOVER_COLOR, CURE_SVG, ctx);
 }
 
 var startGame  = function(e) {
@@ -496,6 +566,27 @@ var startGame  = function(e) {
     createVaccineButton();
     createCureButton();
     window.requestAnimationFrame(update);
+}
+
+var mouseMoved = function(e) {
+    if (vaccine_button)
+        vaccine_button.checkHover(e.offsetX, e.offsetY);
+
+    if (cure_button)
+        cure_button.checkHover(e.offsetX, e.offsetY);
+
+    if (vaccine_button.hover || cure_button.hover)
+        makeMousePointer();
+    else
+        makeMouseSelector();
+}
+
+var makeMousePointer = function() {
+    c.className = "mouse-pointer";
+}
+
+var makeMouseSelector = function() {
+    c.className = "";
 }
 
 var resetGame = function(e) {
@@ -511,6 +602,9 @@ var vaccine_button, cure_button;
 var c = document.getElementById("game-canvas");
 c.width = CANVAS_WIDTH;
 c.height = CANVAS_HEIGHT;
+var ctx = c.getContext("2d");
+
+c.addEventListener("mousemove", mouseMoved);
 
 var start = document.getElementById("start-button");
 start.addEventListener("click", startGame);
@@ -518,4 +612,3 @@ start.addEventListener("click", startGame);
 var reset = document.getElementById("reset-button");
 reset.addEventListener("click", resetGame);
 
-var ctx = c.getContext("2d");
