87a9814fdeebf4e134d403c5294f55f1a689d053
Jenny Baran <jenny.baran@gmail.com>
2015-02-23 16:27:06 -0500

    finished, i think

--- 7/sat/jenny_steven/script.js
+++ 7/sat/jenny_steven/script.js
@@ -1,8 +1,8 @@
-console.log(rawscores);
+//console.log(rawscores);
 
 var data = rawscores["data"];
 
-console.log(data);
+//console.log(data);
 
 var school_scores = [];
 
@@ -18,5 +18,28 @@ for ( var i = 0; i < data.length; i++ ) {
     school_scores.push(temp);
 }
 
-console.log(school_scores);
+//console.log(school_scores);
 
+var math_scores = [];
+
+for ( var i = 0; i < school_scores.length; i++ ) {
+    var temp = school_scores["math"];
+    math_scores.push(temp);
+}
+
+var total_math = 0;
+
+for ( var i = 0; i < math_scores.length; i++ ) {
+    total_math += math_scores[i];
+}
+
+var mean_math = total_math / math_scores.length;
+
+var success_schools = {};
+
+for ( var i = 0; i < school_scores.length; i++ ) {
+    var temp = school_scores[i];
+    if ( temp["math"] > mean_math ) {
+        success_schools[temp["name"]] = temp["math"];
+    }
+}
