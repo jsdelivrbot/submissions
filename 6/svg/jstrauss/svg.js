5982e7e0e4ad2192315448ab5e713cc9e828f0fc
justinstrauss <justinianstrauss@gmail.com>
2015-02-22 22:15:58 -0500

    cool svg

--- 6/svg/jstrauss/svg.js
+++ 6/svg/jstrauss/svg.js
@@ -1,13 +1,4 @@
-var addCircle = function(s,x,y,r,c) {
-    var c1 = document.createElementNS("http://www.w3.org/2000/svg","circle");
-    c1.setAttribute('cx',x);
-    c1.setAttribute('cy',y);
-    c1.setAttribute('r',r);
-    c1.setAttribute('fill',c);
-    c1.addEventListener('click',cClicked);
-    s.appendChild(c1);
-    
-};
+var score = 0;
 
 var clicked = function(e){
         e.preventDefault();
@@ -16,29 +7,14 @@ var clicked = function(e){
         }
 };
 
-var move = function() {
-    var cs = document.getElementsByTagName("circle");
-    for (var i=0;i<cs.length;i++) {
-				var x = parseFloat(cs[i].getAttribute('cx'));
-				var y = parseFloat(cs[i].getAttribute('cy'));
-				var r = parseFloat(cs[i].getAttribute('r'));
-				x = x + 2*Math.random()-1;
-				y = y + 2*Math.random()-1;
-				r = r + 2*Math.random()-1;
-				if (r<5) {r=20;}
-				cs[i].setAttribute('cx',x);
-				cs[i].setAttribute('cy',y);
-				cs[i].setAttribute('r',r);
-    }
-};
-
 var t =0;
 var go = function() {
     if (t==0){
-				t = setInterval(moveBlocks,100);
-    } else {
-				clearInterval(t);
-				t=0;
+		t = setInterval(moveBlocks,20);
+    } 
+    else {
+		clearInterval(t);
+		t=0;
     }
 };
 
@@ -49,16 +25,15 @@ var makeBlock = function(x,y) {
     b1.setAttribute('width',30);
     b1.setAttribute('height',15)
     b1.setAttribute('fill','red');
-//    b1.setAttribute('velocity',{ x: 100, y: 100 });
+    b1.setAttribute('dx',2+Math.random())
     s.appendChild(b1);
 };
 
 var makeDrop = function(x,y) {
-    var d1 = document.createElementNS("http://www.w3.org/2000/svg","rect");
-    d1.setAttribute('x',x);
-    d1.setAttribute('y',y);
-    d1.setAttribute('width',10);
-    d1.setAttribute('height',10)
+    var d1 = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    d1.setAttribute('cx',x);
+    d1.setAttribute('cy',y);
+    d1.setAttribute('r',5);
     d1.setAttribute('fill','blue');
 //    b1.setAttribute('velocity',{ x: 100, y: 100 });
     s.appendChild(d1);
@@ -67,17 +42,45 @@ var makeDrop = function(x,y) {
 var moveBlocks = function() {
     var bs = document.getElementsByTagName("rect");
     for (var i=0; i<bs.length; i++) {
-        console.log("moving");
         var x = parseFloat(bs[i].getAttribute('x'));
-        dx = x + 2 + Math.random();
-        bs[i].setAttribute('cx',x+dx);
+        var dx = parseFloat(bs[i].getAttribute('dx'));
+        if (x < 0 || x > 575){
+            bs[i].setAttribute('dx',dx*-1);
+        }
+        var dx = parseFloat(bs[i].getAttribute('dx'));
+        bs[i].setAttribute('x',x+dx);
     }
+
+    var ds = document.getElementsByTagName("circle");
+    for (var i=0; i<ds.length; i++) {
+        var y = parseFloat(ds[i].getAttribute('cy'));
+        ds[i].setAttribute('cy',y+1);
+        if (y == 600) {
+            score = score - 1;
+        }
+        for (var j = 0; j < bs.length; j++) {
+            if ((Math.abs(parseFloat(ds[i].getAttribute('cx')) - parseFloat(bs[j].getAttribute('x'))) < 20) && (Math.abs(parseFloat(ds[i].getAttribute('cy')) - parseFloat(bs[j].getAttribute('y'))) < 10)) {
+                if (bs[j].getAttribute('fill') == "blue") {
+                    if (confirm("Game over! Your final score is " + score)){
+                        location.reload();
+                    }
+                }
+                else {
+                    bs[j].setAttribute('fill','blue');
+                    score ++;
+                    ds[i].remove();
+                }
+            }
+        }
+    }
+    document.getElementById("points").innerHTML = score.toString();
+
 };
 
 var s = document.getElementById("s");
 s.addEventListener('click',clicked);
 var score = 0;
 for (var x = 100; x < 600; x += 50){
-    makeBlock(580*Math.random()+10,x);
+    makeBlock(565*Math.random()+5,x);
 }
 go();
