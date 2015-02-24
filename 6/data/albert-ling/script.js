eecd757c6b4b5044095d6dddc386cb8e708b96b8
Ling Dong <lingd1944@gmail.com>
2015-02-23 21:43:11 -0500

    FINISHED

--- 6/data/albert-ling/script.js
+++ 6/data/albert-ling/script.js
@@ -1,5 +1,5 @@
 schoolscores = schoolscores.data;
-var temp = []
+var temp = [];
 for (var i = 0;i< schoolscores.length;i++){
     temp.push({
 	'name':schoolscores[i][9],
@@ -12,3 +12,24 @@ for (var i = 0;i< schoolscores.length;i++){
 }
 schoolscores = temp;
 
+var mathscores = [];
+for (var i=0; i< schoolscores.length; i++){
+    if (schoolscores[i].math != "s")
+	mathscores.push(parseInt(schoolscores[i].math));
+}
+
+var mean = 0;
+var sumscores = 0;
+for (var i=0; i<mathscores.length; i++){
+    sumscores = sumscores + mathscores[i];
+}
+
+mean = sumscores/mathscores.length;
+
+var aboveAvg = [];
+for (var i=0; i<schoolscores.length; i++){
+    if (schoolscores[i].math != "s" && parseInt(schoolscores[i].math) > mean)
+	aboveAvg.push(schoolscores[i].name);
+}
+
+console.log(aboveAvg)
