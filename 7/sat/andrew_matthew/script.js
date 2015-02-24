f9b96d9e8a41fe4b91586a506e0fd14365b253ec
Andrew <andrew120.lee@gmail.com>
2015-02-23 20:54:14 -0500

    Finised four parts

--- 7/sat/andrew_matthew/script.js
+++ 7/sat/andrew_matthew/script.js
@@ -18,3 +18,32 @@ for (var q=0; q<rawdata.length;q++){
 };
 
 console.log(school_scores);
+
+math_scores=[];
+for (var i=0;i<school_scores.length;i++){
+    var tmp = parseInt(school_scores[i]["math"]);
+    if (tmp>=0){
+	math_scores.push(tmp);
+    }
+};
+
+console.log(math_scores);
+
+var sum = 0;
+for (var i = 0;i<math_scores.length;i++){
+    sum += math_scores[i];
+};
+
+var mean = sum/math_scores.length;
+
+console.log(mean);
+
+greater = [];
+for (var i = 0;i<math_scores.length;i++){
+    var tmp = parseInt(math_scores[i]);
+    if (tmp > mean){
+	greater.push(tmp);
+    }
+};
+
+console.log(greater);
