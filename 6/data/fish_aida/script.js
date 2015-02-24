e29c5faae9e7ce94f43f70ff1f2e9c425c417593
Aida Piccato <aidapiccato@gmail.com>
2015-02-23 20:39:03 -0500

    1st part

--- 6/data/fish_aida/script.js
+++ 6/data/fish_aida/script.js
@@ -1,16 +1,13 @@
-console.log(raw_scores);
-var raw_scores_data = raw_scores.raw_scores_data;
-console.log(raw_scores_data);
+raw_scores_data = raw_scores.data;
 var school_scores = [];
 for (var i = 0; i < raw_scores_data.length; i++){
 	var t = {
-		code: raw_scores_data[i][8];
-		name: raw_scores_data[i][9];
-		num: raw_scores_data[i][10];
-		eng: raw_scores_data[i][11];
-		math: raw_scores_data[i][12];
-		writing: raw_scores_data[i][13];
+		code: raw_scores_data[i][8],
+		name: raw_scores_data[i][9],
+		num: raw_scores_data[i][10],
+		eng: raw_scores_data[i][11],
+		math: raw_scores_data[i][12],
+		writing: raw_scores_data[i][13]
 	};
 	school_scores.push(t);
 };
-console.log(school_scores);
