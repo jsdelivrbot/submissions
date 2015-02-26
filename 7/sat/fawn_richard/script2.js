b2aa9b81e5fa63f16d6b1aeb138dad1965fd84b7
RzhanRichardZhan <zhan97richard@gmail.com>
2015-02-26 01:13:26 -0500

    functions

---
+++ 7/sat/fawn_richard/script2.js
@@ -0,0 +1,64 @@
+//used Zamansky's instead of rewriting my own.
+
+var rawschooldata = rawdata.data;
+
+var map = function(l,f){
+    var result = [];
+    for (i = 0; i < l.length; i++){
+	result.push(f(l[i]));
+    };
+    return result;
+};
+
+
+var filter = function(l,f){
+    var result = [];
+    for (i = 0; i < l.length; i++){
+	if (f(l[i])){
+	    result.push(l[i]);
+	}
+    };
+    return result;
+};
+
+var reduce = function(l,f,init){
+    var result = init;
+    for (i = 0; i < l.length; i++){
+	result = f(l[i],result);
+    };
+    return result;
+    
+};
+scores = map(rawschooldata,
+	     function(item){
+		 return {
+		     code:item[8],
+		     name:item[9],
+		     num:item[10],
+		     eng:item[11],
+		     math:item[12],
+		     writing:item[13]
+		 };
+	     });
+
+//var rawmath = map(scores,function(item){return item.math;});
+// rawmath = map(rawmath,function(x){return parseInt(x);});
+// var mathscores = filter(rawmath,function(x){return !isNaN(x);});
+
+var rawmath = scores.map(function(x){return x.math}).map(function(x){return parseInt(x)}).filter(function(x){return !isNaN(x)});
+
+//can't install underscore.js
+//var rawmath = _.filter(_.map(_.map(scores,function(x){return x.math}),function(x){parseInt(x)}),function(x){return !isNaN(x);
+
+
+
+//var sum = reduce(mathscores,function(a,b){return a+b;},0);
+//var avg = Math.floor(sum/mathscores.length);
+//console.log(avg);
+
+// var goodmath = filter(scores,function(item){
+//     return parseInt(item.math)>avg;
+// });
+
+//console.log(goodmath);
+console.log(rawmath);
