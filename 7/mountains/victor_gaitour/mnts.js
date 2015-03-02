499538039196e45656f5f8a294c984c2a84acd67
Victor Gaitour <victorgaitour@yahoo.com>
2015-03-02 13:18:25 -0500

    for loop

---
+++ 7/mountains/victor_gaitour/mnts.js
@@ -0,0 +1,21 @@
+var mtn=[20,30,33,40,50,40,20,70,50,110,45];
+var differences =[]
+
+var calculate = function(){
+    for(var i= 0; i<mtn.length-1; i++){
+	differences.push(Math.abs(mtn[i+1]-mtn[i]));
+    }
+    var totalover30=0;
+    for (var i=0; i<differences.length; i++){
+	if (differences[i]>=30){
+	    totalover30++;
+	}
+    }
+    console.log(totalover30);
+    return totalover30;
+}
+
+var calculatenoloop = function(){
+}
+
+    
