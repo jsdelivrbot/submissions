35a79c8ea8254f3cd34791a4bcef9e36f06750b8
Andrew <andrew120.lee@gmail.com>
2015-03-02 18:13:45 -0500

    added mountain hw

---
+++ 7/mountains/andrew_lee/mtn.js
@@ -0,0 +1,21 @@
+var mtn = [20,30,33,40,50,40,20,70,50,110,45];//how many intervals >= 30
+
+console.log(mtn);
+
+var ranges = _.map(mtn,function(value,index){
+    if (index+1 < mtn.length){
+	return Math.abs(value-mtn[index+1]);
+    }
+});
+
+console.log(ranges);
+
+var greater = _.filter(ranges,function(value){
+    return value >= 30;
+}
+
+console.log(greater);
+
+var number = greater.length;
+
+console.log(number);
