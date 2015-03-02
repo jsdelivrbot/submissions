36e04a35b55058698bda2331e09e090125e82864
Alex Libman <kingalex11235@yahoo.com>
2015-03-02 11:47:05 -0500

    done

--- 5/mountains/alex_libman/script.js
+++ 5/mountains/alex_libman/script.js
@@ -2,8 +2,8 @@ var mtnRange = [20,30,33,40,50,40,20,70,50,110,45]
 
 var countMtnDiffs = function(mountains, threshold){
     return _.filter(mountains, function(n, i, list){
-	return Math.abs(n - list[i + 1]) <= threshold;
-    });
+	return Math.abs(n - list[i + 1]) >= threshold;
+    }).length;
 };
 
 console.log(countMtnDiffs(mtnRange, 30));
