c540733db43f97baf7937119d0b09133687105b7
Angela Lin <angela97lin@gmail.com>
2015-03-02 13:57:10 -0500

    hey

--- 7/mountains/angela_lin/mtn.js
+++ 7/mountains/angela_lin/mtn.js
@@ -1,4 +1,19 @@
 mtn = [10, 20, 22, 40, 50, 40, 30, 70, 50, 110, 45]
-
+var count = 0;
 var h = _.map(mtn, function(height){ return height });
 
+var filt = _.filter(h, function(item, index, list){
+    if (list.length % 2 == 0) {
+	if (Math.abs(item - list[index+1]) >= 30)
+	    console.log(item, list[index+1])
+	    count+=1;
+    }
+//	console.log(item, list[index+1]);
+    else {
+	if ((index != list.length-1) && (Math.abs(item - list[index+1]) >= 30))
+	    count+=1;
+    }
+});
+
+console.log(filt);
+console.log(count);
