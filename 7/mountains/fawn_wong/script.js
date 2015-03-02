80ef0d24d4b8238286eb5fbf047d698ded7f4f3a
fawnwong <fawn.wong@live.com>
2015-03-02 13:31:17 -0500

    done i think

--- 7/mountains/fawn_wong/script.js
+++ 7/mountains/fawn_wong/script.js
@@ -1,5 +1,8 @@
 var mtn = [20, 30, 33, 40, 50, 40, 20, 70, 50, 110, 45]
 
-console.log(mtn)
+var intervals = _.chain(mtn)
+    .map(function(x, y){ return Math.abs(x - mtn[y + 1]); })
+    .filter(function(x){ return x >= 30; })
+    .value();
 
-
+console.log(intervals.length);
