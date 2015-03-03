3415cce185be7ad84197959174c99507534832a2
d-tsui <dtsui918@yahoo.com>
2015-03-03 00:58:15 -0500

    done

--- 7/mountains/derek_tsui/mountains.js
+++ 7/mountains/derek_tsui/mountains.js
@@ -1,5 +1,17 @@
 var mts = [20, 30, 33, 40, 50, 40, 20, 70, 50, 10, 45];
-console.log(mts);
 
-var intervals = function(){
-}
+var over = function(data, x){
+  // list of differences
+  diffs = _.map(data,function(n,key){
+      if (key+1 < mts.length) { return Math.abs(n - mts[key+1]); }
+  });
+
+  // which differences > x
+  filt = _.filter(diffs,function(n){
+      return n >= x;
+  });
+
+  return filt.length;
+};
+
+console.log(over(mts,30));
