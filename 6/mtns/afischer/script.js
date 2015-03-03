592cd5ff920f677c5efa64a5f2b40ff318b61e4e
Andrew Fischer <afischer15@mac.com>
2015-03-03 12:14:34 -0500

    oopsies

--- 6/mtns/afischer/script.js
+++ 6/mtns/afischer/script.js
@@ -2,9 +2,13 @@ var mtn = [20,30,33,40,50,40,20,70,50,110,45];
 
 
 var L = _.chain(mtn)
-    .map(function(item,index, mtn){ return Math.abs(mtn[index+1] - item);})
+    .map(function(item,index, mtn){
+        return Math.abs(mtn[index+1] - item);
+    })
     //Altitude differences
-    .filter(function(item){ return item >= 30;})
+    .filter(function(item){
+        return item >= 30;
+    })
     .value();
 
 console.log("Number of steap drops: ")
