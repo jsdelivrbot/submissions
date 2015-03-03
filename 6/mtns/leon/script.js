11018bdd387c81285c2dd274123d4e5e0adf04c9
voidyknight <leonfrajmund@gmail.com>
2015-03-03 01:03:23 -0500

    Sdfadsf

---
+++ 6/mtns/leon/script.js
@@ -0,0 +1,8 @@
+function mountains(list, diff){
+    var count = 0;
+    _.each(list, function(val, index){ 
+	    if i < list.length {
+		    if Math.abs(list[i+1]-x >= 30){
+			    count += 1;}}});
+    return count;
+}
