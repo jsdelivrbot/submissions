0ca4687afa8242da89d6f7120ae57d32b4c00324
Andrew Fischer <afischer15@mac.com>
2015-03-03 12:11:55 -0500

    forgot to push >:(

---
+++ 6/mtns/afischer/script.js
@@ -0,0 +1,11 @@
+var mtn = [20,30,33,40,50,40,20,70,50,110,45];
+
+
+var L = _.chain(mtn)
+    .map(function(item,index, mtn){ return Math.abs(mtn[index+1] - item);})
+    //Altitude differences
+    .filter(function(item){ return item >= 30;})
+    .value();
+
+console.log("Number of steap drops: ")
+console.log(L.length);
