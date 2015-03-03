3b8c500ed694dbcf74fc233346c9b7a334f3196b
Michael Huang <mikester591@gmail.com>
2015-03-02 23:05:54 -0500

    Added Files

---
+++ 5/mountains/Michael_Huang/mountain.js
@@ -0,0 +1,9 @@
+var mtn= ['10','40','75','80','111','90','121','20']
+
+var countRange = function(ranges){
+    return  _.filter(ranges,function(value,index,range){if ((range[index+1]-value) >= 30){return value;}}).length
+    }
+		    
+
+
+console.log(countRange(mtn));
