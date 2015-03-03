e84a8929f9ddf7a6e91ada8fcac62e7b175d21eb
Michael Huang <mikester591@gmail.com>
2015-03-02 23:09:23 -0500

    Small fix

--- 5/mountains/Michael_Huang/mountain.js
+++ 5/mountains/Michael_Huang/mountain.js
@@ -1,7 +1,7 @@
 var mtn= ['10','40','75','80','111','90','121','20']
 
 var countRange = function(ranges){
-    return  _.filter(ranges,function(value,index,range){if ((range[index+1]-value) >= 30){return value;}}).length
+    return  _.filter(ranges,function(value,index,range){if ((Math.abs(range[index+1]-value)) >= 30){return value;}}).length
     }
 		    
 
