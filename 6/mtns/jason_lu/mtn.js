16561668971218b8cb9d0856b5e21d64564011b5
jasonluX13 <jasonluX13@gmail.com>
2015-03-03 22:25:22 -0500

    mtn

--- 6/mtns/jason_lu/mtn.js
+++ 6/mtns/jason_lu/mtn.js
@@ -1,2 +1,6 @@
-var mtn = [20,30,33,40,50,40,20,50,80,70,45,10,20];
+var mtns = [20,30,33,40,50,40,20,50,80,70,45,10,20];
 
+var a = _.map(mtns, function(x,i,mtns){return Math.abs(x - mtns[i+1]);});
+a = a.slice(0,a.length-1);
+
+var steep = _.filter(a, function(x){return x>=30;});
