aff9d162522207fb11d10d2c0646dc58d3e345ae
Applesauce136 <harrison.chiu.136@gmail.com>
2015-03-11 11:33:31 -0400

    typos and things

--- 5/backbone1/harrison_ivy/static/app.js
+++ 5/backbone1/harrison_ivy/static/app.js
@@ -1,54 +1,54 @@
 console.log("HELLO");
 
 var PlaceView = Backbone.View.extend({
-		el:"#place",
-		//template: _.template("<tr><td><%= name %></td><td><%= rating %></td></tr>"),
-		template: _.template($("#place_template").html()),
-		events: {
-				"click #del" : function(e) {
-						this.remove();
-				},
-				"click #up" : function(e) {
-						var r = this.model.get("rating");
-						r = parseInt(r);
-						r = r + 1;
-						this.model.set('rating',r);
-						this.render();
-				},
-				"click #down" : function(e) {
-						var r = this.model.get("rating");
-						r = parseInt(r);
-						r = r - 1;
-						this.model.set('rating',r);
-						this.render();
-				},
-		},
-		initialize:function(){
-				this.render();
-		},
-		render: function(){
-				var e = this.template(this.model.toJSON());
-				this.$el.empty();
-				this.$el.append(e);
-				return this;
-		}
+    el:"#place",
+    //template: _.template("<tr><td><%= name %></td><td><%= rating %></td></tr>"),
+    template: _.template($("#place_template").html()),
+    events: {
+        "click #del" : function(e) {
+            this.remove();
+        },
+        "click #up" : function(e) {
+            var r = this.model.get("rating");
+            r = parseInt(r);
+            r = r + 1;
+            this.model.set('rating',r);
+            this.render();
+        },
+        "click #down" : function(e) {
+            var r = this.model.get("rating");
+            r = parseInt(r);
+            r = r - 1;
+            this.model.set('rating',r);
+            this.render();
+        },
+    },
+    initialize:function(){
+        this.render();
+    },
+    render: function(){
+        var e = this.template(this.model.toJSON());
+        this.$el.empty();
+        this.$el.append(e);
+        return this;
+    }
 
 
 });
 
 
 var Place = Backbone.Model.extend({
-		initialize: function() {
-				this.on({"change":function() {
-						console.log("Changed"+this.toJSON())}});
-		},
-		defaults:{'name':'name goes here',
-							'rating':0},
-		validate:function(attrs,options){
-				if (isNaN(attrs.rating)){
-						return "Rating must be numeric";
-				}
-		}
+    initialize: function() {
+        this.on({"change":function() {
+            console.log("Changed"+this.toJSON())}});
+    },
+    defaults:{'name':'name goes here',
+              'rating':0},
+    validate:function(attrs,options){
+        if (isNaN(attrs.rating)){
+            return "Rating must be numeric";
+        }
+    }
 });
 
 var p1 = new Place({name:"Terry's", rating:5});
