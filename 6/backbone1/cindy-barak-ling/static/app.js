59025b1aac1de4a213a1027c2389ba1d2b5f4af3
zhoubw <bz2118111@gmail.com>
2015-03-11 22:51:05 -0400

    download scripts not needed

--- 6/backbone1/cindy-barak-ling/static/app.js
+++ 6/backbone1/cindy-barak-ling/static/app.js
@@ -1,59 +1,59 @@
 console.log("HELLO");
 
 var Place = Backbone.Model.extend({
-		showchange: function(){
-						console.log("Changing: "+this.toString());
-		},
-		initialize:function(){
-				this.on("change",this.showchange);
-		},
-		destroy: function() {
-				this.off("change",showchange);
-		},
-		defaults: {
-				name :"Place name",
-				rating:0
-		},
-		validate : function(attrs,options) {
-				if (isNaN(attrs.rating)){
-						return "need number";
-				}
-		}
+    showchange: function(){
+	console.log("Changing: "+this.toString());
+    },
+    initialize:function(){
+	this.on("change",this.showchange);
+    },
+    destroy: function() {
+	this.off("change",showchange);
+    },
+    defaults: {
+	name :"Place name",
+	rating:0
+    },
+    validate : function(attrs,options) {
+	if (isNaN(attrs.rating)){
+	    return "need number";
+	}
+    }
 });
 
 var PlaceView = Backbone.View.extend({
-		el			: "#place",
-		template	:  _.template($("#place_template").html()),
-		events:{
-				"click #up" : function(e) {
-						var r = this.model.get("rating");
-						r = parseInt(r);
-						r = r + 1;
-						this.model.set("rating",r);
-						this.render();
-				},
-				
-				"click #down" : function(e) {
-						var r = this.model.get("rating");
-						r = parseInt(r);
-						r = r - 1;
-						this.model.set("rating",r);
-						this.render();
-				},
-				
-				"click #del" : function(e) {
-						this.remove();
-				}
-		},
-		initialize:function(){
-				this.render();
-		},
-		render: function() {
-				var e = this.template(this.model.toJSON());
-				this.$el.empty();
-				this.$el.append(e);
-				return this;
-		}
+    el			: "#place",
+    template	:  _.template($("#place_template").html()),
+    events:{
+	"click #up" : function(e) {
+	    var r = this.model.get("rating");
+	    r = parseInt(r);
+	    r = r + 1;
+	    this.model.set("rating",r);
+	    this.render();
+	},
+	
+	"click #down" : function(e) {
+	    var r = this.model.get("rating");
+	    r = parseInt(r);
+	    r = r - 1;
+	    this.model.set("rating",r);
+	    this.render();
+	},
+	
+	"click #del" : function(e) {
+	    this.remove();
+	}
+    },
+    initialize:function(){
+	this.render();
+    },
+    render: function() {
+	var e = this.template(this.model.toJSON());
+	this.$el.empty();
+	this.$el.append(e);
+	return this;
+    }
 
 });
 
