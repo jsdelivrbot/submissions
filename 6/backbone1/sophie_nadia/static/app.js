58af1f4605eab121ae027b98b40e9bee0acfe933
Sophie Gershon <sophgersh@gmail.com>
2015-03-12 22:30:30 -0400

    two templates, edit and view

--- 6/backbone1/sophie_nadia/static/app.js
+++ 6/backbone1/sophie_nadia/static/app.js
@@ -21,9 +21,9 @@ var Place = Backbone.Model.extend({
   }
 });
 
-var PlaceView = Backbone.View.extend({
-    el : "#place",
-    template :  _.template($("#place_template").html()),
+var EditView = Backbone.View.extend({
+    el : "#edit",
+    template :  _.template($("#edit_template").html()),
     events : {
 	"click #up" : function(e) {
 	    var r = this.model.get("rating");
@@ -51,14 +51,28 @@ var PlaceView = Backbone.View.extend({
     render: function() {
 	var e = this.template(this.model.toJSON());
 	this.$el.empty();
-     this.$el.append(e);
+	this.$el.append(e);
 	return this;
     }
     
 });
 
 
+var PlaceView = Backbone.View.extend({
+    el : "#place",
+    template :  _.template($("#place_template").html()),
+    initialize:function(){
+	this.render();
+    },
+    render: function() {
+	var e = this.template(this.model.toJSON());
+	this.$el.empty();
+	this.$el.append(e);
+	return this;
+    }
+});
+
+
 var p1 = new Place({name:"Terry's",rating:5});
-var p2 = new Place({name:"Ferry's",rating:8});
 var v1 = new PlaceView({model:p1});
-var v2 = new PlaceView({model:p2});
+var e1 = new EditView({model:p1});
