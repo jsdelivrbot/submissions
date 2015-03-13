7588b3140103234c66cdd73a9155d0c4a35ccb93
fawnwong <fawn.wong@live.com>
2015-03-12 23:36:41 -0400

    yeah you better be sorry genji

--- 7/backbone1/fawn_genji/static/app.js
+++ 7/backbone1/fawn_genji/static/app.js
@@ -15,20 +15,14 @@ var Place = Backbone.Model.extend({
 					},
 	defaults	:	{
 						name	: "defaultName",
-						rating	: 0
+						rating	: 0,
+						review	: ""
 					}	
 });
 
 var PrettyView = Backbone.View.extend({
 	el			:	"#pretty",
 	template	:	_.template( $("#pretty_template").html()),
-	events		:	{
-						/* Still haven't decided on how to do real-time updating with multiple views */
-						/* Sorry fawn */
-						/* Also sorry because I'm ocd about tabbing */
-						/* much sorry fawn */
-	
-					}
 	render		:	function() {
 						/* There's an error here but I have no idea how to fix it */
 						/* Sorry fawn */
@@ -36,10 +30,14 @@ var PrettyView = Backbone.View.extend({
 						var e = this.template(this.model.toJSON());
 						this.$el.empty();
 						this.$el.append(e);
+						console.log(this.model.review);
 						return this;
 					},	
 	initialize	:	function() {
 						this.render();
+						this.listenTo(this.model, 'change', this.render);
+						console.log("initializing pretty view");
+						this.listenTo(this.model, 'destroy', this.render);
 					}
 });
 
@@ -67,11 +65,18 @@ var UglyView = Backbone.View.extend({
 												this.model.set("rating",r);;
 												this.render();
 										  	},
+						"click .edit"	:	function(e) {
+												var text = $("textarea").val();
+												console.log(text);
+												this.model.set("review", text);
+											},
+						"click .erase"	: 	function(e) {
+												$("textarea").val('');
+												var text = $("textarea").val();
+												this.model.set("review", text);
+											},			
 					},
 	render		:	function() {
-						/* There's an error here but I have no idea how to fix it */
-						/* Sorry fawn */
-						/* Text me if you can't figure something out and need me to do things*/
 						var e = this.template(this.model.toJSON());
 						this.$el.empty();
 						this.$el.append(e);
@@ -84,6 +89,6 @@ var UglyView = Backbone.View.extend({
 
 
 var p = new Place({name: "Terry's", rating: 5});
-var pView = new PrettyView(p);
-var uView = new UglyView(p);
+var pView = new PrettyView({model:p});
+var uView = new UglyView({model:p});
 
