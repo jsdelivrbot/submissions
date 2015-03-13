77da26365505ae4f20b5a0b0c1683fc49fbd3ab0
Junhao Dong <junhao.dong96@gmail.com>
2015-03-13 01:16:47 -0400

    finish

--- 5/backbone1/eric-kolbusz_junhao/static/app.js
+++ 5/backbone1/eric-kolbusz_junhao/static/app.js
@@ -1,8 +1,7 @@
 var DataView = Backbone.View.extend({
-    el:"#data",
+    el: "#data",
     template: _.template($("#data_template").html()),
-    initialize:function(){
-        this.listenTo(this.model,"edit",this.render);
+    initialize: function(){
 	this.render();
     },
     render: function(){
@@ -14,7 +13,7 @@ var DataView = Backbone.View.extend({
 });
 
 var EditView = Backbone.View.extend({
-    el:"#edit",
+    el: "#edit",
     template: _.template($("#edit_template").html()),
     events: {
 	"click #up" : function(e) {
@@ -31,16 +30,23 @@ var EditView = Backbone.View.extend({
 	    this.model.set('rating',r);
 	    this.render();
 	},
-	"click #desc" : function(e) {
+	"click #edit" : function(e) {
+	    var desc = newDesc.value;
+	    console.log(desc); //
+	    this.model.set('desc', desc);
+	    this.render();
 	}
     },
-    initialize:function(){
+    view: null,
+    initialize: function(options){
+	_.extend(this, _.pick(options, "view"));
 	this.render();
     },
     render: function(){
 	var e = this.template(this.model.toJSON());
 	this.$el.empty();
 	this.$el.append(e);
+	this.view.render();
 	return this;
     }
 });
@@ -48,20 +54,19 @@ var EditView = Backbone.View.extend({
 var Place = Backbone.Model.extend({
     initialize: function() {
 	this.on({"change":function() {
-	    console.log("Changed"+this.toJSON())}});
+	    console.log("Changed"+this.toJSON());}});
     },
-    defaults:{'name':'name goes here',
-	      'rating':0,
-	      'desc':'review goes here'},
+    defaults:{
+	'name':'name goes here',
+     	'rating':0,
+	'desc':'review goes here'
+    },
     validate:function(attrs,options){
-	if (isNaN(attrs.rating)){
+	if (isNaN(attrs.rating))
 	    return "Rating must be numeric";
-	}
     }
 });
 
-var p1 = new Place({name:"Ferry's", rating:7});
-var d1 = new DataView({model:p1});
-var e1 = new EditView({model:p1});
-
-console.log("DONE");
+var p1 = new Place({name:"Ferry's", rating:7, desc:"fake terry's; long line in the afternoon; many kids"});
+var dv1 = new DataView({model:p1});
+var ev1 = new EditView({model:p1, view: dv1});
