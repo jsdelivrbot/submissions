1e5686aa2d8b96c5a0ca2d12ef56510d5e39f6ed
Andreas <awang289@gmail.com>
2015-03-17 23:12:16 -0400

    done

--- 5/backbone1/Andreas_Morgenstern/static/app.js
+++ 5/backbone1/Andreas_Morgenstern/static/app.js
@@ -7,20 +7,30 @@ var PlaceView = Backbone.View.extend({
 		events: {
 				"click #del" : function(e) {
 						this.remove();
+						v2.remove();
 				},
 				"click #up" : function(e) {
 						var r = this.model.get("rating");
 						r = parseInt(r);
 						r = r + 1;
 						this.model.set('rating',r);
-						this.render();
+						this.render();	
+						v2.render();
+
 				},
 				"click #down" : function(e) {
-						var r = this.model.get("rating");
+				                var r = this.model.get("rating");
 						r = parseInt(r);
 						r = r - 1;
 						this.model.set('rating',r);
 						this.render();
+						v2.render();
+				},
+				"click #change": function(e) {
+				    var r = document.getElementById("d").value;
+				    this.model.set("description", r);
+				    this.render();
+				    v2.render();
 				},
 		 
 		},
@@ -51,9 +61,21 @@ var Place = Backbone.Model.extend({
 				}
 		}
 });
-var EditView = Backbone.View.extend({
-    
+var SecondView = Backbone.View.extend({
+    	el:"#place1",
+		//template: _.template("<tr><td><%= name %></td><td><%= rating %></td></tr>"),
+		template: _.template($("#place_template1").html()),
+		initialize:function(){
+				this.render();
+		},
+		render: function(){
+				var e = this.template(this.model.toJSON());
+				this.$el.empty();
+				this.$el.append(e);
+				return this;
+		}
 });
 var p1 = new Place({name:"Terry's", rating:5});
 var p2 = new Place({name:"Ferry's", rating:7});
 var v1 = new PlaceView({model:p1});
+var v2 = new SecondView({model:p1});
