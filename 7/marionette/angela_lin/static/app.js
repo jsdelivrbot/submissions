cb8678386fbc4107ad3ad7af921f8904d961304f
Angela Lin <angela97lin@gmail.com>
2015-03-28 19:33:45 -0400

    base

---
+++ 7/marionette/angela_lin/static/app.js
@@ -0,0 +1,14 @@
+console.log("story testy")
+
+var App = new Marionette.Application();
+
+App.addRegions({
+
+
+
+});
+
+App.on("start", function(){
+    console.log("start");
+    //Backbone.history.start();
+}
