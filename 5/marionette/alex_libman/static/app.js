3402938e3cd3497f83cebad19e8c71b930468e48
Alex Libman <kingalex11235@yahoo.com>
2015-03-29 00:38:59 -0400

    work

--- 5/marionette/alex_libman/static/app.js
+++ 5/marionette/alex_libman/static/app.js
@@ -1,1 +1,51 @@
-var app = new Marionette.Application();
+var App = new Marionette.Application();
+
+App.addRegions({
+    blogArea: "#blogs"
+});
+
+App.on("start", function(){
+    console.log("Blog engine started.");
+
+    var blogsview = new App.BlogsView({collection:testBlogs});
+    App.blogArea.show(blogsview);
+});
+
+
+App.BlogView = Marionette.ItemView.extend({
+    template: "#blogs-template",
+    events: {
+		"click #delete" : function(){
+			this.remove();
+		}
+	},
+	modelEvents: {
+		"change" : function(){
+            this.render();
+        }
+	}
+});
+
+App.BlogsView = Marionette.CollectionView.extend({
+    childView: App.BlogView
+});
+
+
+var Blog = Backbone.Model.extend({});
+var Blogs = Backbone.Collection.extend({
+    model: Blog
+});
+
+var testBlog = new Blog({
+    title: "Testing",
+    name: "John Doe",
+    content: "Blah blah blah"
+});
+var testBlog2 = new Blog({
+    title: "Testing2",
+    name: "John Doe2",
+    content: "Blah blah 2blah"
+});
+var testBlogs = new Blogs([testBlog,testBlog2]);
+
+App.start();
