c224fa3f792eb433b2751d3df0fe3e38858b0f71
ezlotnik <elisez97@gmail.com>
2015-03-29 23:22:45 -0400

    progress-ish?

---
+++ 5/marionette/elise_zlotnikova/static/app.js
@@ -0,0 +1,11 @@
+console.log("HELLO");
+console.log("HEYO2");
+var count = 0;
+var current = null;
+var App = new Marionette.Application();
+
+App.addRegions({
+    firstRegion:"#first-region",
+    secondRegion:"#second-region"
+});
+
