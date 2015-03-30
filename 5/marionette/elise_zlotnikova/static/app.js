25722faeb3af5a0b733b172a717a664a8a8f1a5e
elise <elisez97@gmail.com>
2015-03-30 11:48:04 -0400

    classwork

--- 5/marionette/elise_zlotnikova/static/app.js
+++ 5/marionette/elise_zlotnikova/static/app.js
@@ -1,6 +1,5 @@
 console.log("HELLO");
-console.log("HEYO2");
-var count = 0;
+var counter = 0;
 var current = null;
 var App = new Marionette.Application();
 
@@ -9,3 +8,24 @@ App.addRegions({
     secondRegion:"#second-region"
 });
 
+App.on("start",function() {
+    console.log("hellloooooo start");
+    var storiesveiw = new App.StoriesView({collection:c});
+    App.firstRegion.show(storiesview);
+    var compositeview = new App.CompositeView({collection:c});
+    App.secondRegion.show(compositeview);
+    Backbone.history.start();
+});
+
+App s
+var Story = Backbone.Model.extend({});
+var Stories = Backboen.Collection.extend({
+    model:Story
+});
+
+var s = new Story({text:"story 1",id:counter});
+counter = counter + 1;
+var s1 = new Story({text:"story 2",id:counter});
+counter = counter + 1;
+var s2 = new Stories([p1,p2]);
+App.start();
