fae7da1e83a492a293ca9bc0201de4cdc37d449a
DionisWang <dwan1016@aol.com>
2015-03-31 02:34:59 -0400

    update

--- 7/marionette/dionis_wang/static/app.js
+++ 7/marionette/dionis_wang/static/app.js
@@ -1,35 +1,66 @@
 console.log("HELLO!!!");
 
 var App = new Marionette.Application();
-var Turn = BackBone.Model.extend();
-var Turns = BackBone.Collection.extend({
-    model: Turn
+var Turn = Backbone.Model.extend({
+    urlRoot:'/story',
+    idAttribute:'_id',
+    id:'_id',
 });
+var Turns = Backbone.Collection.extend({
+    model: Turn,
+    url:'/game',
+    initialize:function() {
+	this.fetch();
+    }
+});
+var c = new Turns();
 
 App.TurnView = Marionette.ItemView.extend({
-    template: "#history-template"
-});
-
-App.HistoryView = Marionette.CollectionView.extend({
-    childView : App.TurnView
-});
-
-App.StoryView = Marionette.CompositeView.extend({
-    template: "#story-template",
-    tagName:"p",
-    events:{
-	"click #submit": function(){
-	    var n = $("#line").val();
-	    if(n.length>0){
-		this.fetch().add(new Turn({content:n}));
-		this.save();
-		$("#line").val("");
-	    }
-	}
-    },
+    template: "#history-template",
+    tagName:"li",
     modelEvents:{
 	"change":function(){
 	    this.render();
+	}
+    }
+});
+App.OtherView = Marionette.ItemView.extend({
+    template: "#other-template",
+    tagName:"span",
+    modelEvents:{
+	"change":function(){
+	    this.render();
+	}
+    }
+});
+App.HistoryView = Marionette.CollectionView.extend({
+    childView : App.TurnView,
+});
+App.StoryView = Marionette.CompositeView.extend({
+    template: "#story-template",
+    childView : App.OtherView,
+    view: c,
+    tagName:"p",
+    events:{
+	"click #add": function(e){
+	    e.preventDefault();
+	    var line = $("#entry").val()
+	    var n = $("#name").val()
+	    if(n.length<1){
+		n='anonymous';
+	    }
+	    var that=this;
+	    if(line.length>0){
+		$("#entry").val("");
+		var m = new Turn({name:n,content:line});
+		m.save(m.toJSON(),{success:function(m,r){
+		    if (r.result.content==n.content && r.result.name==n.name){
+			that.collection.add(m);
+			that.render();
+			console.log("success");
+		    }	
+		}});
+	    }
 	}
     }
 });
@@ -42,11 +73,11 @@ App.addRegions({
 App.on("start", function(){
     console.log("STARTING!!!");
     
-    var storyView = new App.StoryView({collection:db});
-    App.story.show(storyView);
-    
-    var historyView = new App.HistoryView({collection:db});
-    App.history.show(historyView);
+    var storyView = new App.StoryView({collection:c});
+    App.story.show(storyView);     
+
+    var historyView = new App.HistoryView({collection:c});
+    App.history.show(historyView);  
 });
 
 App.start();
