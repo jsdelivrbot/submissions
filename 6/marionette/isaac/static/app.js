f2b5cfedff646e10ea7c7dc1dc6a2c213bc6fb38
IsaacGluck <isaacbgluck@gmail.com>
2015-03-30 22:46:39 -0400

    ok

--- 6/marionette/isaac/static/app.js
+++ 6/marionette/isaac/static/app.js
@@ -1,26 +1,44 @@
 var App = new Marionette.Application();
+
+App.addRegions({
+	main: "#main"
+});
+
+Todo = Backbone.Model.extend({});
+
+Todo_list = Backbone.Collection.extend({
+  model: Todo
+});
+
+
+
 App.TaskView = Marionette.ItemView.extend({
 	template: "#task-template",
-	tagName: "tr"
+	tagName: "tr",
+	className: "todo_item"
 });
+
 App.CompView = Marionette.CompositeView.extend({
 		template : "#composite-template",
 		childView : App.TaskView,
 		childViewContainer : "tbody"
 });
-App.addRegions({
-	main: "#main"
-});
+
 App.on("start", function(){
 	var compview = new App.CompView({collection:c});
 	App.main.show(compview);
 });
 
 var Task = Backbone.Model.extend();
+
 var Tasks = Backbone.Collection.extend({
 	model: Task
 });
+
 var t = new Task({task:"Water the plants"});
+
 var c = new Tasks([t]);
+
 console.log("hello");
+
 App.start();
