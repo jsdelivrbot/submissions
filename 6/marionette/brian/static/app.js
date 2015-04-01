27d34948d0ba8b2a9c8c857ccc2b78d6af52bcd8
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2015-03-31 20:11:27 -0400

    finished styling, basic assignment completed

--- 6/marionette/brian/static/app.js
+++ 6/marionette/brian/static/app.js
@@ -9,12 +9,11 @@ App.on("start",function(){
     console.log('starting app');
     var entriesView = new App.EntriesView({collection:e});
     App.entries.show(entriesView);
-    //var commentsView= new App.CommentsView({collection: new Comments({'entry':''})});
-    //App.comments.show(commentsView);
 });
 
+/***********/
 /* Entries */
-
+/***********/
 App.EntryView = Marionette.ItemView.extend({
     template : '#entry-template',
     tagName : "tr",
@@ -26,12 +25,10 @@ App.EntryView = Marionette.ItemView.extend({
     },
     events : {
 	'click' : function() {
-	    //console.log(this.model);
 	    var that = this;
 	    var comments = new Comments();
 	    comments.entry = this.model.get('id');
 	    var commentsView = new App.CommentsView({collection: comments, model: that.model});
-	    console.log(commentsView);
 	    App.comments.show(commentsView);
 	}
     }
@@ -65,14 +62,14 @@ var Entries = Backbone.Collection.extend({
     url: '/entries',
     initialize: function(){
 	this.fetch(function(d){
-	    console.log(d);
 	    this.render();
 	});
     }
 });
 
+/************/
 /* Comments */
-
+/************/
 App.CommentView = Marionette.ItemView.extend({
     template : '#comment-template',
     tagName : "tr",
@@ -92,7 +89,6 @@ App.CommentsView = Marionette.CompositeView.extend({
     childView : App.CommentView,
     childViewContainer : 'tbody',
     initialize: function(){
-	console.log(this.model);
 	var that = this;
 	//this.collection.fetch({data: $.param({'name':that.get('entry')})});
 	this.collection.setEntry(this.model.id);
@@ -104,10 +100,8 @@ App.CommentsView = Marionette.CompositeView.extend({
 	'click #addcomment' : function(){
 	    var n = $('#newcommentname').val();
 	    if (n.length > 0){
-		console.log(n);
 		var newC = new Comment({content:n, entry:this.model.id});
 		//console.log(this.collection);
-		console.log(newC);
 		this.collection.add(newC);
 		newC.save();
 		$('#newcommentname').val('');
@@ -125,7 +119,6 @@ var Comments = Backbone.Collection.extend({
     setEntry: function(n){
 	console.log(this.entry);
 	this.fetch({data: $.param({'entry':n})},function(d){
-	    console.log(d);
 	    this.render();
 	});
     }
