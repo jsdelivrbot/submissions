9e637d1fac4d148b91052f18396d200b8f8d01d5
unknown <m.vasilkin@gmail.com>
2015-04-01 03:29:08 -0400

    Tried mongo, failed at mongo

--- 6/marionette/Maria/static/app.js
+++ 6/marionette/Maria/static/app.js
@@ -1,29 +1,27 @@
 var App = new Marionette.Application();
 
 App.addRegions({
-		firstRegion: "#first-region",
-		secondRegion: "#second-region",
-		thirdRegion: "#third-region",
-		fourthRegion: "#fourth-region"
+    firstRegion : "#first-region"
 });
-
 
 App.on("start", function(){
     console.log("Initializing");
 
     var addview = new App.AddView({collection:c});
-    App.fourthRegion.show(addview);
+    App.firstRegion.show(addview);
 
-    var staticview = new App.StaticView();
-    App.firstRegion.show(staticview);
 });
 
 
+
+
+
+//for instructions
 App.StaticView = Marionette.ItemView.extend({
     template : "#static-template"
 });
 
-
+//the story is separated into lines
 App.LineView = Marionette.ItemView.extend({
     template : "#line",
     tagName : "li",
@@ -31,19 +29,21 @@ App.LineView = Marionette.ItemView.extend({
 	"change" : function(){
 	    this.render();}
     }
-});
+})
 
+//the StoryView is an aggregation of the lines
 App.StoryView = Marionette.CollectionView.extend({
     template : "#story-template",
     childView : App.LineView,
 });
 
+//for adding to the story
 App.AddView = Marionette.CompositeView.extend({
     template : "#add-template",
     childView : App.LineView,
     childViewContainer : "ol",    
     events : {
-	"click #new_item" : function(){
+	"click #add" : function(){
 	    var n = $("#newline").val();
 	    if (n.length != 0){
 		this.collection.add(new Line({l:n}));
@@ -65,6 +65,10 @@ var StoryView = Backbone.Collection.extend({
 });
 
 
-var beginning = new Line({l:Milk});
-var c = new StoryView([beginning]);
+var first = new Line({l:"Milk"});
+var c = new StoryView([first]);
+
+
+
+
 App.start();
