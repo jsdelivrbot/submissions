0710b59aa326c7ccd61c5b2b97576a69698fea19
koprty <koprty@users.noreply.github.com>
2015-04-03 21:55:11 -0400

    bson to json conversion mess - just upgraded pymongo

--- 7/marionette/lise_ho/app.py
+++ 7/marionette/lise_ho/app.py
@@ -1,21 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, json, request
 from pymongo import MongoClient
+#import json
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
-
+client = MongoClient()
+db = client['mydb']
+print "hello"
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/ca")
+@app.route("/data", methods=["GET"])
+def data():
+    db.members.insert({"name":"Richar"})
+    L=[]
+    for y in db.members.find():
+        try:
+            y["_id"] = int(y["priority"])
+        except:
+            y["_id"] = 20
+        L.append(y)
+    members = [x for x in L]
+   
+    return dumps(members)
+
+def remove(_id):
+    return collection.remove({"_id":_id})
+
+
+@app.route("/ca", methods=['GET','POST','DELETE','PUT'])
+@app.route("/ca", methods=['GET','POST','DELETE','PUT'])
 def ind():
+    method = request.method
+    mem = request.get_json()
+    if method == "POST":
+        #add to mongodb
+        _id = mem["priority"]
+        print method, mem, _id
+        db.members.insert(mem)
+    #data()
+   
+    
+    
     return render_template("family.html")
 
 
 if __name__ == "__main__":
-    client = MongoClient()
-    db = client['mydb']
+    print "HASDF"
     app.debug = True
     app.run(host="0.0.0.0", port=8000)
 
--- 7/marionette/lise_ho/static/app.js
+++ 7/marionette/lise_ho/static/app.js
@@ -1,5 +1,33 @@
 console.log("ADSFADSFADSFASDFAD");
 var App = new Marionette.Application();
+var names = "Samuel"
+var image ="http://vignette2.wikia.nocookie.net/tamagotchi/images/8/8d/Tamagotchi_blue.png/revision/latest?cb=20110906021758";
+var Tamagachi = Backbone.Model.extend({
+    idAttributes:"_id",
+    url:"/ca"
+});
+// names = prompt("Welcome to the family of Tamagchi! \nPlease enter the name of the first member of your Tamagachi's family (presumably your name).\n");
+var tama = new Tamagachi({
+    happiness:5,
+    name:names, //names comes from in line script in html file
+    hunger:2,
+    priority:10,
+    img_src:image
+});
+tama.save();
+var TamaCollection = Backbone.Collection.extend({
+    model:Tamagachi,
+    comparator:name,
+    
+    initialize:function(){
+	this.fetch();
+
+    }
+});
+
+
+var n = 2;
+var c = new TamaCollection([tama]);
 
 App.addRegions({
     firstRegion:"#first-region",
@@ -20,6 +48,9 @@ App.on("start", function(){
     var staticView = new App.StaticView();
     App.fourthRegion.show(staticView);
     
+    var addView = new App.AddView({collection:c});
+    App.thirdRegion.show(addView);
+
     var compositeView = new App.CompView({collection:c, model:tama});
     App.secondRegion.show(compositeView);
  //   var tamaView = new App.TamaView({collection:c,model:tama});
@@ -61,20 +92,31 @@ App.StartView = Marionette.ItemView.extend({
 	    console.log("adding NEW NAME");	
 	    // ADD TO MONGO DB
 	    this.render();
-	},
-	"click #addnew": function(){
-	    var x = new Tamagachi({
-		name: $("#name2").val(),
-		name: $("#happiness").val(),
-		name: $("#hunger").val(),
-	    });
-	    console.log(x.attributes.name);
-	    c.render();
-	    // ADD TO MONGO DB
-	    this.render();
 	}	
     }
 });
+App.AddView = Marionette.ItemView.extend({
+    template:"#add-template",
+    events:{
+	"click #addnew": function(){
+	    var x = new Tamagachi({
+		name: $("#name2").val(),
+		happiness:  Math.floor((Math.random() * 5)+3),
+		hunger: Math.floor((Math.random() * 5)+1),
+		_id:n,
+		img_src: image,
+	    });
+	    n += 1;
+	    c.add(x);
+	    console.log(c,x.get("name"));
+	    
+	    // ADD TO MONGO DB
+	    this.render();
+	}
+
+    }
+})
+
 App.CompView = Marionette.CompositeView.extend({
     template:"#composite-template",
     childView:App.TamaStatView,
@@ -90,29 +132,6 @@ App.CompView = Marionette.CompositeView.extend({
 	}
     }
 });
-
-var Tamagachi = Backbone.Model.extend();
-var tama = new Tamagachi({
-    happiness:5,
-    name:names, //names comes from in line script in html file
-    hunger:2,
-    img_src:"http://vignette2.wikia.nocookie.net/tamagotchi/images/8/8d/Tamagotchi_blue.png/revision/latest?cb=20110906021758"
-});
-var tama2 = new Tamagachi({
-    happiness:9,
-    name:"Samuel", //names comes from in line script in html file
-    hunger:20,
-    img_src:"http://vignette2.wikia.nocookie.net/tamagotchi/images/8/8d/Tamagotchi_blue.png/revision/latest?cb=20110906021758"
-});
-var TamaCollection = Backbone.Collection.extend({
-    model:Tamagachi,
-    comparator:name
-});
-
-var c = new TamaCollection([tama, tama2]);
-
-console.log(tama.attributes.name)
-
 
 
 App.start();
